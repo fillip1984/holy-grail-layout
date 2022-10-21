0976ea85b42cbede01441972a095f8b1b66eaf41
fillip1984 <fillip1984@gmail.com>
2022-10-21 19:19:27 -0400

    component-ized backdrop

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Admin from "./components/Admin/Admin";
 import Home from "./components/Home/Home";
+import Backdrop from "./components/navigation/Backdrop";
 import BottomDrawer from "./components/navigation/BottomDrawer";
 import Footer from "./components/navigation/Footer";
 import SideDrawer from "./components/navigation/SideDrawer";
@@ -48,15 +49,12 @@ const App = () => {
         setBottomDrawerVisible={setBottomDrawerVisible}
       />
 
-      <div
-        className={`backdrop fixed top-0 left-0 right-0 bottom-0 z-[900] select-none bg-white/50 backdrop-invert backdrop-opacity-30 ${
-          sideDrawerVisible || bottomDrawerVisible ? "visible" : "hidden"
-        }`}
-        onClick={() => {
-          // TODO: would be nice if you didn't have to close every drawer here
-          setSideDrawerVisible(false);
-          setBottomDrawerVisible(false);
-        }}></div>
+      <Backdrop
+        sideDrawerVisible={sideDrawerVisible}
+        bottomDrawerVisible={bottomDrawerVisible}
+        setSideDrawerVisible={setSideDrawerVisible}
+        setBottomDrawerVisible={setBottomDrawerVisible}
+      />
     </div>
   );
 };
---
+++ src/components/navigation/Backdrop.tsx
@@ -0,0 +1,28 @@
+interface BackdropProps {
+  sideDrawerVisible: boolean;
+  bottomDrawerVisible: boolean;
+  setSideDrawerVisible: (state: boolean) => void;
+  setBottomDrawerVisible: (state: boolean) => void;
+}
+
+const Backdrop = ({
+  sideDrawerVisible,
+  bottomDrawerVisible,
+  setSideDrawerVisible,
+  setBottomDrawerVisible,
+}: BackdropProps) => {
+  return (
+    <div
+      className={`backdrop fixed top-0 left-0 right-0 bottom-0 z-[900] select-none bg-white/50 backdrop-invert backdrop-opacity-30 ${
+        sideDrawerVisible || bottomDrawerVisible ? "visible" : "hidden"
+      }`}
+      onClick={() => {
+        // TODO: would be nice if you didn't have to close every drawer here
+        setSideDrawerVisible(false);
+        setBottomDrawerVisible(false);
+      }}
+    />
+  );
+};
+
+export default Backdrop;
